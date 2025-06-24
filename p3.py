0f5e642b61dab822df2733bd59f2bbb733829c06
tilak kadlaskar <tilakkadlaskar66@gmail.com>
2025-06-24 17:21:52 +0530

    dot-product-example-for-one-neuron

--- p3.py
+++ p3.py
@@ -1,19 +1,24 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-bias = [2, 3, 0.5]
+output = np.dot(inputs, weights) + bias
+print(output)
 
 
-'''
-layer_output = []
-for neuron_weights, neuron_bias in zip(weights, bias):
-    neuron_output =0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input * weight
-    neuron_output += neuron_bias
-    layer_output.append(neuron_output)
-print(layer_output)
-'''
+
+
+
+
+
+
+
+
+# weights = [[0.2, 0.8, -0.5, 1.0],
+#            [0.5, -0.91, 0.26, -0.5],
+#            [-0.26, -0.27, 0.17, 0.87]]
+
+# bias = [2, 3, 0.5]
+
